517e3418184fcdde4c1db1505108068c82291452
Vrishabh <vrishabhmishra7@gmail.com>
2020-08-20 23:40:54 +0530

    Updates

---
+++ precache-manifest.fa558c2c2d1b20dae271615f2079fc4b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8013d6d265d10b6f61128db82742f831",
+    "url": "/CollegeDuniya/index.html"
+  },
+  {
+    "revision": "acfdac9457fde461d536",
+    "url": "/CollegeDuniya/static/css/main.d562afd2.chunk.css"
+  },
+  {
+    "revision": "ef8511027671f897b477",
+    "url": "/CollegeDuniya/static/js/2.ae70e209.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CollegeDuniya/static/js/2.ae70e209.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acfdac9457fde461d536",
+    "url": "/CollegeDuniya/static/js/main.37ad8fd2.chunk.js"
+  },
+  {
+    "revision": "412a81d84ff9fa7a236c",
+    "url": "/CollegeDuniya/static/js/runtime-main.ec5ae02d.js"
+  },
+  {
+    "revision": "619ee97eae1c5e93f5d68a9157b2466c",
+    "url": "/CollegeDuniya/static/media/college_02.619ee97e.jpg"
+  }
+]);
